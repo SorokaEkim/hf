fd7823efc33bf6315ff1fc96a5aca45488291ac2
Soroka <soroka.ekim@gmail.com>
2023-03-20 23:53:06 +0300

    install livewire

---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Hello Maks</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-Route::get('/', [AdminController::class, 'getTeachers']);
+// Route::get('/', [AdminController::class, 'getTeachers']);
+Route::view('/', 'welcome');
 
 
